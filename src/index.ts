5919cfe440378bd43ff350772f057af9e2b91cc8
Antony74 <akb@akb.me.uk>
2026-04-25 22:04:57 +0100

    Simplify with new checkText function

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,22 @@
-import checkmm, { getCheckmmState, setCheckmmState } from 'checkmm';
+import { checkText } from 'checkmm';
 
 const editor = document.getElementById('editor');
 const validateButton = document.getElementById('validate');
 const output = document.getElementById('output');
 
-if (editor && validateButton && output && editor instanceof HTMLTextAreaElement) {
-    const initialState = getCheckmmState();
-
+if (
+    editor &&
+    validateButton &&
+    output &&
+    editor instanceof HTMLTextAreaElement
+) {
     fetch('demo0.mm')
         .then((response) => response.text())
         .then((text) => (editor.value = text));
 
     validateButton.onclick = async () => {
-        setCheckmmState(initialState);
-        checkmm.data = editor.value;
-
         try {
-            const fileInclusion = checkmm.readtokenstofileinclusion();
-
-            if (fileInclusion) {
-                throw new Error(
-                    `File inclusions not supported in this example`,
-                );
-            }
-
-            checkmm.processtokens();
+            await checkText(editor.value);
             output.textContent = 'Validated OK';
         } catch (e) {
             if (e instanceof Error) {
